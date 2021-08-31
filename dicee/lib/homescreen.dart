1e798611567a11f7c76b74df0aad002c04e5cd11
savanismit <savanismit11@gmail.com>
2021-08-31 12:05:40 +0530

    Animation Added

--- dicee/lib/homescreen.dart
+++ dicee/lib/homescreen.dart
@@ -8,14 +8,45 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   int LeftDiceIndex = 1;
   int RightDiceIndex = 1;
-  void roll() {
-    setState(() {
-      LeftDiceIndex = Random().nextInt(6) + 1;
-      RightDiceIndex = Random().nextInt(6) + 1;
+  late AnimationController _controller;
+  late CurvedAnimation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animate();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  animate() {
+    _controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+    animation = CurvedAnimation(parent: _controller, curve: Curves.bounceOut);
+    animation.addListener(() {
+      setState(() {});
     });
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          LeftDiceIndex = Random().nextInt(6) + 1;
+          RightDiceIndex = Random().nextInt(6) + 1;
+        });
+        _controller.reverse();
+      }
+    });
+  }
+
+  void roll() {
+    _controller.forward();
   }
 
   @override
@@ -36,6 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(15.0),
                     child: Image(
+                        height: 200 - (animation.value) * 200,
                         image:
                             AssetImage('images/dice-png-$LeftDiceIndex.png')),
                   ),
@@ -46,6 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(15.0),
                     child: Image(
+                        height: 200 - (animation.value) * 200,
                         image:
                             AssetImage('images/dice-png-$RightDiceIndex.png')),
                   ),
